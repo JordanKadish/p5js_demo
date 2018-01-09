1d2ed69124f52c12d600bc34bafa820b0faf9f0c
Jordan <jordankadish93@gmail.com>
2018-01-09 05:21:14 +0200

    managed to asynchronously load the birds.json file correctly into birds.js. Logged in console as POC

--- birds.js
+++ birds.js
@@ -1,7 +1,12 @@
+//the setup() and draw() functions are part of the p5 api
 function setup(){
+    loadJSON("birds.json", fetchData);//asynchronous call to load the json stored data in birds.json
+}
 
+function fetchData(data){//when this func is called, "data" contains the info sent from the calling object
+    console.log(data)
 }
 
 function draw(){
-	
+
 }
