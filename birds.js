89883210bbcf02131004661b20bd3ef920f421f2
Jordan <jordankadish93@gmail.com>
2018-01-09 07:06:16 +0200

    Info now displays as expected. May add animations eventually

--- birds.js
+++ birds.js
@@ -1,24 +1,22 @@
 var birdData;
 //the setup() and draw() functions are part of the p5 api
 function setup(){//this gets injected into the body tag
-    noCanvas();
+    noCanvas();//ensure no instantiation of a canvas element
     loadJSON("birds.json", fetchData);//asynchronous call to load the json stored data in birds.json
-                                      //this could be done with via a preload, but I wanted to test asynch
+                                      //this could be done with via a preload, but I wanted to test asynch (will replace with url...)
 }
 
 function fetchData(data){//when this func is called, "data" contains the info sent from the calling object
     birdData=data;
-    if(birdData){//we only work with the data if it exists
         var birds = birdData.birds;
         var birdObjs = birds.length;//efficiency, don't repeatedly check birds.length...
         for (var loopFam = 0;loopFam < birdObjs; loopFam++) {
             createElement('h1',birds[loopFam].family);//p5 way to create headings
             var members = birds[loopFam].members;
-            for (member in members) {
-                createDiv(member);
+            for (member in members) {//loop through the array of birds in each family
+                createDiv(members[member]);//create a p5.dom.min.js div for each one
             }
         }
-    }
 }
 
 function draw(){//this looping function constantly renders to screen for animations etc
