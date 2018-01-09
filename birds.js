84332582372b371a9284e3605cc6ee3c345f54b1
Jordan <jordankadish93@gmail.com>
2018-01-09 06:46:45 +0200

    Fixed an error where I needed p5.dom.min.js but forgot to download it as a separate file!

--- birds.js
+++ birds.js
@@ -1,12 +1,25 @@
+var birdData;
 //the setup() and draw() functions are part of the p5 api
-function setup(){
+function setup(){//this gets injected into the body tag
+    noCanvas();
     loadJSON("birds.json", fetchData);//asynchronous call to load the json stored data in birds.json
+                                      //this could be done with via a preload, but I wanted to test asynch
 }
 
 function fetchData(data){//when this func is called, "data" contains the info sent from the calling object
-    console.log(data)
+    birdData=data;
+    if(birdData){//we only work with the data if it exists
+        var birds = birdData.birds;
+        var birdObjs = birds.length;//efficiency, don't repeatedly check birds.length...
+        for (var loopFam = 0;loopFam < birdObjs; loopFam++) {
+            createElement('h1',birds[loopFam].family);//p5 way to create headings
+            var members = birds[loopFam].members;
+            for (member in members) {
+                createDiv(member);
+            }
+        }
+    }
 }
 
-function draw(){
-
+function draw(){//this looping function constantly renders to screen for animations etc
 }
